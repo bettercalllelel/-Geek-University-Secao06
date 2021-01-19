fc60f7f8729036430917cd4282ad703356cf8a5c
bettercalllelel <60786916+bettercalllelel@users.noreply.github.com>
2021-01-18 23:09:00 -0300

    Create 09.py

---
+++ 09.py
@@ -0,0 +1,9 @@
+prim = int(input('Diga um número para iniciar a contagem:\n'))
+num = int(input('Diga a quantidade de números ímpares subsequentes a demonstrar:\n'))
+
+i = 0
+
+while i < num:
+    print(prim+2)
+    i = i + 1
+    prim = prim + 2
